0616085b90b22744072ead651eb9aa6c3c999cd4
chuanshi <chuanshi@qunhemail.com>
2019-10-14 22:59:15 +0800

    Initial Commit

---
+++ Programming-C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Programming-C
+//
+//  Created by 杨笑天 on 2019/10/14.
+//  Copyright © 2019 杨笑天. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
