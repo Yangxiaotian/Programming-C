51d7bd5ce28756170522ac704c57cf769822f178
yangxiaotian <stdlib@foxmail.com>
2019-10-15 13:38:34 +0800

    insert_sort complete

--- Programming-C/algorithm/insert_sort.c
+++ Programming-C/algorithm/insert_sort.c
@@ -7,7 +7,34 @@
 //
 
 #include <stdio.h>
-void insertSort(int *A, n)
+void insertSort(int *A, int n)
 {
-    for
+    for(int i=1; i<n; i++)
+    {
+        int key = A[i];
+        int j = i-1;
+        while(j >= 0 && key < A[j])
+        {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = key;
+    }
 }
+void play(void)
+{
+    int n;
+    printf("请输入数组大小：");
+    scanf("%d", &n);
+    int *A = (int*)malloc(sizeof(int)*n);
+    printf("请输入数据以空格或换行分隔：\n");
+    for(int i=0; i<n; i++)
+        scanf("%d", A+i);
+    //A = {5,2,1,3,2};
+    //A[0]=5, A[1]=2, A[2]=1,A[3]=3,A[4]=4;
+    insertSort(A, n);
+    for(int i=0; i<n; i++)
+        printf("%d\t", A[i]);
+}
+
+
