0d41cad293bc202d898c62215bcd934b07b1c0f4
chuanshi <chuanshi@qunhemail.com>
2019-10-15 10:03:28 +0800

    1

--- Programming-C/main.c
+++ Programming-C/main.c
@@ -6,10 +6,10 @@
 //  Copyright © 2019 杨笑天. All rights reserved.
 //
 
-#include <stdio.h>
+#include "wordcnt/wordcnt.c"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    wordcnt();
     return 0;
 }
