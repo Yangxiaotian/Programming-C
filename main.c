8537a0610e58c637a2aead04e5d8f4f8ca2d6aa8
chuanshi <chuanshi@qunhemail.com>
2019-11-15 23:56:37 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Programming-C
+//
+//  Created by 杨笑天 on 2019/10/14.
+//  Copyright © 2019 杨笑天. All rights reserved.
+//
+
+#include "algorithm/quick_sort.c"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    play();
+    return 0;
+}
